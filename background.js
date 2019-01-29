afbac84d0a8817b659963b5618f2331d044e2719
mauriciocap@gmail.com <mauriciocap@gmail.com>
2019-01-28 23:43:10 -0300

    FUNCIONA: fillem up leyendo de url

--- background.js
+++ background.js
@@ -10,3 +10,31 @@ chrome.contextMenus.create({
   "onclick" : onCtxClick
 });
 //A: cree el context menu para regiones editables
+
+chrome.runtime.onMessage.addListener( function(req,sender,sendResponse) {
+	if(req.fill && req.id) {
+		chrome.tabs.executeScript(req.id,{code: `
+			//e= document.activeElement;
+			valores= ${JSON.stringify(req.fill)};
+			fillParent= document.body;
+			if (valores["__feu_id__"]) { //A:xpath,nombre criptico para que no nos reclamen
+				var x= document.evaluate(valores["__feu_id__"],document, null, XPathResult.ANY_TYPE,null); 
+				fillParent= x.iterateNext();
+				console.log("FILL EM UP PARENT",valores["__feu_id__"],fillParent);
+			}
+			for (k in valores) { 
+				var e= document.getElementById(k) || fillParent.querySelector("[name='"+k+"']"); 
+				console.log("FILL EM UP FOUND",k,e);
+				if (e && (e.tagName=="INPUT" || e.tagName=="SELECT")) { 
+					console.log("FILL EM UP SET",valores[k],e);
+					e.value= valores[k] 
+				} 
+				else {
+					console.error("FILL EM UP WILL NOT SET (for security reasons)",valores[k],e);
+				}
+			}
+		`}, function (comoFue) {
+			sendResponse();
+		});
+	}
+});
--- fillEmUp.js
+++ fillEmUp.js
@@ -1,9 +1,45 @@
+async function get_from_url(opts) {
+	return await fetch(opts.url, {
+		method: (opts.method=="POST") ? "POST" : "GET", // *GET, POST, PUT, DELETE, etc.
+		headers: opts.headers,
+		body: opts.data !=null ? JSON.stringify(opts.data): null,
+		mode: "cors", // no-cors, cors, *same-origin //A: cors para ir a otros dominios
+		cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+		credentials: "omit", // include, *same-origin, omit
+		redirect: "follow", // manual, *follow, error
+		referrer: "no-referrer", // no-referrer, *client
+	});
+}
+
+async function get_data(txt) {
+	var m;
+	if (m= txt.match(/^[\s\r\n]*(http\S+)/)) {
+		let res= await get_from_url({url: m[1]});
+		xx= res;
+		console.log("RES",res);
+		let rtxt= await res.text();
+		if (rtxt.match(/^[\s\r\n]*\{/)) { return JSON.parse(rtxt); }
+		else { alert("ERROR FORMATO"); console.log("TXT",m[1], rtxt); }
+	}
+	else if (txt.match(/^[\s\r\n]*\{/)) {
+		return JSON.parse(txt);
+	}
+}
+
+
 document.addEventListener("DOMContentLoaded", function () {
-	var tabId= window.location.hash.substring(1);
+	var tabId= parseInt(window.location.hash.substring(1));
 	var b_cancel_el= document.getElementById("cancel");
 	var b_fill_el= document.getElementById("fill");
 	var ta_el= document.getElementById("ta");
 	
 	b_cancel_el.onclick= () => window.close();
-	b_fill_el.onclick= () => alert("dale!");
+	b_fill_el.onclick= async () => {
+		let data= await get_data(ta_el.value);
+		if (data) {
+			chrome.runtime.sendMessage({fill: data, id: tabId}, function (error) {
+				if (!error) { window.close(); }
+			})
+		}
+	};
 });
